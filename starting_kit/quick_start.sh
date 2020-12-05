6b9974a3590ef0bc5cda3536e0d43a0abb9e0052
Adrian <adrian.el_baz@ens-paris-saclay.fr>
2020-12-05 01:08:43 +0100

    Updated version of the forked meta-dataset repository

--- starting_kit/quick_start.sh
+++ starting_kit/quick_start.sh
@@ -8,7 +8,8 @@ conda activate $ENV_NAME
 
 cd ../../
 # Clone and install meta-dataset package (in metadl's ROOT)
-git clone https://github.com/google-research/meta-dataset.git
+git clone https://github.com/ebadrian/meta_dataset
+#git clone https://github.com/google-research/meta-dataset.git
 
 wget -c https://competitions.codalab.org/my/datasets/download/57327142-2155-4b37-9ee7-74820f56c812 -O omniglot.zip
 # Unzip data 
@@ -21,7 +22,7 @@ pip install -r requirements.txt
 
 # Installing meta-dataset package
 cd ../meta-dataset
-cp ../metadl/misc/setup.py .
+#cp ../metadl/misc/setup.py .
 pip install -e .
 
 # Install tensorflow addons 
