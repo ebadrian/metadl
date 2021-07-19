19042e4f3c8d2f9f1624fb620b0ecb68e35c3795
Zhengying Liu <zhengying.liu@polytechnique.edu>
2021-07-19 23:42:37 +0800

    Release cpu-latest

--- docker/release.sh
+++ docker/release.sh
@@ -7,4 +7,6 @@ VERSION=`cat VERSION`
 
 docker push $USERNAME/$IMAGE:gpu-$VERSION
 docker push $USERNAME/$IMAGE:gpu-latest
+docker push $USERNAME/$IMAGE:cpu-$VERSION
+docker push $USERNAME/$IMAGE:cpu-latest
 
