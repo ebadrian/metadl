cac9b960d7df2f6853e79adb401787961636a10e
Zhengying Liu <zhengying.liu@polytechnique.edu>
2021-08-02 17:53:02 +0800

    Fix number of episodes

--- metadl/core/scoring/scoring.py
+++ metadl/core/scoring/scoring.py
@@ -342,7 +342,7 @@ def scoring(argv):
 
         logging.debug('Score on {} : {}'.format(k, score))
         logging.debug('Results : {}'.format(results[:20]))
-        if(k > nbr_episodes):
+        if(k == nbr_episodes - 1):
             break
     def mean_confidence_interval(data, confidence=0.95):
         a = 1.0 * np.array(data)
@@ -359,8 +359,6 @@ def scoring(argv):
                     extract_elapsed_time(saved_model_dir))
 
     # Update detailed results page
-    # TODO (@ebadrian): fetch task_name for each task and show it in the generated
-    #   figure if necessary and possible.
     task_name = None
     plot_detailed_results_figure(results, score_dir, task_name=task_name)
     write_scores_html(score_dir)
