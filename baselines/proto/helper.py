df3f0f7bc72707d8c703451dacbcd1de271cdd51
ebadrian <eb.adrian8@gmail.com>
2021-08-04 11:52:29 +0200

    Update proto baseline. Compatible 128 image size

--- baselines/proto/helper.py
+++ baselines/proto/helper.py
@@ -6,24 +6,34 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten,\
 from tensorflow.keras.initializers import GlorotUniform
 
 tf.random.set_seed(1234)
-def conv_net(nbr_classes, img_size = 28):
+def conv_net(nbr_classes, img_size = 128):
      """Reproduce the CNN used in the MAML paper. It was originally designed in
      Vinyals and al. (2016) .
      Conv layers kernels are initialized with Glorot Uniform by default."""
 
      model = Sequential()
-     model.add(Conv2D(64, (3, 3), strides = (1, 1), activation='relu',\
+     model.add(Conv2D(128, (3, 3), strides = (1, 1), activation='relu',\
           input_shape=(img_size, img_size, 3),
           kernel_initializer=GlorotUniform(seed=1234)))
      model.add(BatchNormalization())
      model.add(MaxPooling2D((2, 2)))
 
-     model.add(Conv2D(64, (3, 3), strides = (1, 1), activation='relu',
+     model.add(Conv2D(128, (3, 3), strides = (1, 1), activation='relu',
           kernel_initializer=GlorotUniform(seed=1234)))
      model.add(BatchNormalization())
      model.add(MaxPooling2D((2, 2)))
 
-     model.add(Conv2D(64, (3, 3), strides = (1, 1), activation='relu',
+     model.add(Conv2D(128, (3, 3), strides = (1, 1), activation='relu',
+          kernel_initializer=GlorotUniform(seed=1234)))
+     model.add(BatchNormalization())
+     model.add(MaxPooling2D((2, 2)))
+
+     model.add(Conv2D(128, (3, 3), strides = (1, 1), activation='relu',
+          kernel_initializer=GlorotUniform(seed=1234)))
+     model.add(BatchNormalization())
+     model.add(MaxPooling2D((2, 2)))
+
+     model.add(Conv2D(128, (3, 3), strides = (1, 1), activation='relu',
           kernel_initializer=GlorotUniform(seed=1234)))
      model.add(BatchNormalization())
      model.add(MaxPooling2D((2, 2)))
